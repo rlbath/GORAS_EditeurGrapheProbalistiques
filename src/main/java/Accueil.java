bc323f38a9c875a49433f33ab6e9bc6dd420512b
REMI JAUZION <remi.jauzion@iut.local>
2022-10-25 09:08:52 +0200

    Amélioration de l'interface graphique

--- src/main/java/Accueil.java
+++ src/main/java/Accueil.java
@@ -25,115 +25,122 @@ public class Accueil extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jPanel2 = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jLabel1 = new javax.swing.JLabel();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenu3 = new javax.swing.JMenu();
-        jMenu4 = new javax.swing.JMenu();
-
-        jMenuItem1.setText("jMenuItem1");
-
-        jMenuItem2.setText("jMenuItem2");
+        panelPalette = new javax.swing.JPanel();
+        panelPropriétés = new javax.swing.JPanel();
+        zoneDessin = new javax.swing.JScrollPane();
+        menuBarre = new javax.swing.JMenuBar();
+        menuGraphe = new javax.swing.JMenu();
+        nouveauGraphe = new javax.swing.JMenuItem();
+        enregistrerSous = new javax.swing.JMenuItem();
+        ouvrir = new javax.swing.JMenuItem();
+        enregistrer = new javax.swing.JMenuItem();
+        menuEdition = new javax.swing.JMenu();
+        undo = new javax.swing.JMenuItem();
+        redo = new javax.swing.JMenuItem();
+        menuTraitements = new javax.swing.JMenu();
+        menuAide = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
         setPreferredSize(new java.awt.Dimension(1280, 720));
+        getContentPane().setLayout(null);
 
-        jPanel2.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        jPanel2.setForeground(new java.awt.Color(204, 204, 204));
+        panelPalette.setBackground(new java.awt.Color(204, 204, 204));
+        panelPalette.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        panelPalette.setForeground(new java.awt.Color(204, 204, 204));
+        panelPalette.setLayout(null);
+        getContentPane().add(panelPalette);
+        panelPalette.setBounds(0, 0, 210, 250);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 212, Short.MAX_VALUE)
+        panelPropriétés.setBackground(new java.awt.Color(204, 204, 204));
+        panelPropriétés.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        javax.swing.GroupLayout panelPropriétésLayout = new javax.swing.GroupLayout(panelPropriétés);
+        panelPropriétés.setLayout(panelPropriétésLayout);
+        panelPropriétésLayout.setHorizontalGroup(
+            panelPropriétésLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 208, Short.MAX_VALUE)
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 313, Short.MAX_VALUE)
-        );
-
-        jPanel1.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 211, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 262, Short.MAX_VALUE)
+        panelPropriétésLayout.setVerticalGroup(
+            panelPropriétésLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 248, Short.MAX_VALUE)
         );
 
-        jScrollPane1.setBackground(new java.awt.Color(255, 255, 255));
+        getContentPane().add(panelPropriétés);
+        panelPropriétés.setBounds(0, 250, 210, 250);
 
-        jLabel1.setText("Caca");
-        jScrollPane1.setViewportView(jLabel1);
+        zoneDessin.setBackground(new java.awt.Color(255, 255, 255));
+        zoneDessin.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        zoneDessin.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        getContentPane().add(zoneDessin);
+        zoneDessin.setBounds(206, 0, 680, 500);
 
-        jMenu1.setText("Graphes");
-        jMenuBar1.add(jMenu1);
+        menuGraphe.setText("Graphes");
 
-        jMenu2.setText("Edition");
-
-        jMenuItem3.setText("UnDo");
-        jMenuItem3.setToolTipText("");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+        nouveauGraphe.setText("Nouveau graphe");
+        nouveauGraphe.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
+                nouveauGrapheActionPerformed(evt);
             }
         });
-        jMenu2.add(jMenuItem3);
+        menuGraphe.add(nouveauGraphe);
 
-        jMenuItem4.setText("ReDo");
-        jMenu2.add(jMenuItem4);
+        enregistrerSous.setText("Enregistrer sous");
+        menuGraphe.add(enregistrerSous);
 
-        jMenuBar1.add(jMenu2);
+        ouvrir.setText("Ouvrir");
+        menuGraphe.add(ouvrir);
 
-        jMenu3.setText("Traitements");
-        jMenuBar1.add(jMenu3);
+        enregistrer.setText("Enregistrer");
+        menuGraphe.add(enregistrer);
 
-        jMenu4.setText("Aide");
-        jMenuBar1.add(jMenu4);
+        menuBarre.add(menuGraphe);
 
-        setJMenuBar(jMenuBar1);
+        menuEdition.setText("Edition");
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 666, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addComponent(jScrollPane1)
-        );
+        undo.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Z, java.awt.event.InputEvent.CTRL_MASK));
+        undo.setText("UnDo");
+        undo.setToolTipText("");
+        undo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                undoActionPerformed(evt);
+            }
+        });
+        menuEdition.add(undo);
+
+        redo.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Y, java.awt.event.InputEvent.CTRL_MASK));
+        redo.setText("ReDo");
+        redo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                redoActionPerformed(evt);
+            }
+        });
+        menuEdition.add(redo);
+
+        menuBarre.add(menuEdition);
+
+        menuTraitements.setText("Traitements");
+        menuBarre.add(menuTraitements);
+
+        menuAide.setText("Aide");
+        menuBarre.add(menuAide);
+
+        setJMenuBar(menuBarre);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+    private void undoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_undoActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem3ActionPerformed
+    }//GEN-LAST:event_undoActionPerformed
+
+    private void redoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_redoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_redoActionPerformed
+
+    private void nouveauGrapheActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nouveauGrapheActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_nouveauGrapheActionPerformed
 
     /**
      * @param args the command line arguments
@@ -171,18 +178,19 @@ public class Accueil extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenu jMenu3;
-    private javax.swing.JMenu jMenu4;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JMenuItem enregistrer;
+    private javax.swing.JMenuItem enregistrerSous;
+    private javax.swing.JMenu menuAide;
+    private javax.swing.JMenuBar menuBarre;
+    private javax.swing.JMenu menuEdition;
+    private javax.swing.JMenu menuGraphe;
+    private javax.swing.JMenu menuTraitements;
+    private javax.swing.JMenuItem nouveauGraphe;
+    private javax.swing.JMenuItem ouvrir;
+    private javax.swing.JPanel panelPalette;
+    private javax.swing.JPanel panelPropriétés;
+    private javax.swing.JMenuItem redo;
+    private javax.swing.JMenuItem undo;
+    private javax.swing.JScrollPane zoneDessin;
     // End of variables declaration//GEN-END:variables
 }
